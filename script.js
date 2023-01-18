0d1ab9b77667eee30245e5f093af4b2610e1d94b
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-01-18 22:38:04 +0500

    site project finished

--- script.js
+++ script.js
@@ -92,7 +92,55 @@ function toggleMute() {
   }
 }
 // Change Playback Speed -------------------------- //
+function changeSpeed(){
+  video.playbackRate = speed.value
+}
 // Full screen ------------------------------------ //
+/* View in fullscreen */
+function openFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    /* Firefox */
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    /* IE/Edge */
+    element.msRequestFullscreen();
+  }
+  video.classList.add('video-fullscreen');
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE/Edge */
+    document.msExitFullscreen();
+  }
+  video.classList.remove('video-fullscreen');
+}
+
+let fullscreen = false;
+
+// Toggle ful screen
+function toggleFullscreen(){
+  if (!fullscreen) {
+    openFullscreen(player)
+  } else {
+    closeFullscreen()
+  }
+  fullscreen = !fullscreen
+}
 // Event listener
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
@@ -101,3 +149,5 @@ video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener('change', changeSpeed)
+fullscreenBtn.addEventListener('click', toggleFullscreen)
