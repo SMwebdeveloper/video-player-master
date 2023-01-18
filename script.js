2b8a7021b9a13d84a8760ae9180a37d487cc814c
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-01-18 21:59:05 +0500

    volume

--- script.js
+++ script.js
@@ -29,36 +29,60 @@ function togglePlay() {
 }
 
 // On video End, show play button icon
-video.addEventListener('ended', showPlayIcon)
+video.addEventListener("ended", showPlayIcon);
 
 // Calculate display time format
-function displayTime(time){ 
-  const minutes = Math.floor(time / 60)
-  let seconds = Math.floor(time % 60)
-  seconds = seconds > 9 ? seconds : `0${seconds}`
-  return `${minutes}:${seconds}`
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
 }
 
 // Progress Bar ----------------------------------- //
 function updateProgress() {
-  progressBar.style.width = `${(video.currentTime / video.duration)* 100}%`
-  currentTime.textContent = `${displayTime(video.currentTime)} /`
-  duration.textContent = `${displayTime(video.duration)}`
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
 }
 
-// Click to seek within the video 
-function setProgress(e){
+// Click to seek within the video
+function setProgress(e) {
   const newTime = e.offsetX / progressRange.offsetWidth;
-  progressBar.style.width = `${newTime * 100}%`
-  video.currentTime = newTime * video.duration
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
   console.log(e);
 }
 // Volume Controls -------------------------------- //
+// Volume Bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  // Rounding volume up or down
+  if(volume < 0.1){
+    volume = 0;
+  }
+  if(volume > 0.9){
+    volume = 1
+  }
+  volumeBar.style.width = `${volume * 100}%`
+  video.volume = volume
+  console.log(volume);
+  //Change icon depending on volume
+  volumeIcon.className = ``
+  if (volume > 0.7) {
+    volumeIcon.classList.add('fas', 'fa-volume-up')
+  } else if(volume < 0.7 && volume > 0){
+    volumeIcon.classList.add('fas', 'fa-volume-down')
+  } else if (volume === 0){
+    volumeIcon.classList.add('fas', 'fa-volume-off')
+  }
+}
 // Change Playback Speed -------------------------- //
 // Full screen ------------------------------------ //
 // Event listener
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
-video.addEventListener('timeupdate', updateProgress)
-video.addEventListener('canplay', updateProgress)
-progressRange.addEventListener('click', setProgress)
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
