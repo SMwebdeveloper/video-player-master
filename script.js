f327000c09b03f7f129f92ee11d2ff2453028659
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-01-18 14:20:24 +0500

    progres bar finished

--- script.js
+++ script.js
@@ -42,7 +42,16 @@ function displayTime(time){
 // Progress Bar ----------------------------------- //
 function updateProgress() {
   progressBar.style.width = `${(video.currentTime / video.duration)* 100}%`
-  displayTime(64)
+  currentTime.textContent = `${displayTime(video.currentTime)} /`
+  duration.textContent = `${displayTime(video.duration)}`
+}
+
+// Click to seek within the video 
+function setProgress(e){
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`
+  video.currentTime = newTime * video.duration
+  console.log(e);
 }
 // Volume Controls -------------------------------- //
 // Change Playback Speed -------------------------- //
@@ -52,3 +61,4 @@ playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 video.addEventListener('timeupdate', updateProgress)
 video.addEventListener('canplay', updateProgress)
+progressRange.addEventListener('click', setProgress)
